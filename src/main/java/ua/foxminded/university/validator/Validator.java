ac3d7f538fb353c7d2164d1e2745b6a362aabd9e
Bohdan Bilyk <bohdanbilyk1001@gmail.com>
2023-06-22 21:38:37 +0200

    Added tests

--- src/main/java/ua/foxminded/university/validator/Validator.java
+++
@@ -1,9 +0,0 @@
-package ua.foxminded.university.validator;
-
-import ua.foxminded.university.exceptions.ValidationException;
-
-public interface Validator {
-    void validateData(String email, String firstName, String lastName) throws ValidationException;
-    
-    void validateEmail(String email) throws ValidationException;
-}
