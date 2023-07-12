d4ee328ac39c600b2e61c096f5a25e188668133e
Bohdan Bilyk <bohdanbilyk1001@gmail.com>
2023-07-12 16:34:23 +0200

    Created interface GroupRepository
    Changed constructor and methods
    
    Removed extra ones classes
    
    Changed methods of service
    
    Removed extra one method
    
    Changed method

--- src/main/java/ua/foxminded/university/service/impl/GroupServiceImpl.java
+++ src/main/java/ua/foxminded/university/service/impl/GroupServiceImpl.java
@@ -3,7 +3,7 @@ package ua.foxminded.university.service.impl;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import lombok.AllArgsConstructor;
-import ua.foxminded.university.dao.GroupDao;
+import ua.foxminded.university.dao.repository.GroupRepository;
 import ua.foxminded.university.entity.Group;
 import ua.foxminded.university.exceptions.ValidationException;
 import ua.foxminded.university.service.GroupService;
@@ -13,23 +13,23 @@ import ua.foxminded.university.validator.ValidatorGroup;
 @AllArgsConstructor
 public class GroupServiceImpl implements GroupService {    
     private final ValidatorGroup validatorGroup;
-    private final GroupDao groupDao;
+    private final GroupRepository groupRepository;
     
     @Override
     public void register(String groupName) throws ValidationException {
 	validatorGroup.validateGroupName(groupName);
-	groupDao.save(new Group(groupName));
+	groupRepository.save(new Group(groupName));
     }
     
     @Override
     public void updateGroupName(Group group) throws ValidationException {
 	validatorGroup.validateGroupName(group.getGroupName());
 	
-	groupDao.update(group);
+	groupRepository.save(group);
     }
 
     @Override
     public List<Group> getGroupsWithLessEqualsStudentCount(int studentCount) {
-	return groupDao.getGroupsWithLessEqualsStudentCount(studentCount);
+	return groupRepository.getGroupsWithLessEqualsStudentCount(studentCount);
     } 
 }
