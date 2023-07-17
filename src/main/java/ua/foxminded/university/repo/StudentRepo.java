26815a650661d48c6f851e017b215c412720b8c7
Bohdan Bilyk <bohdanbilyk1001@gmail.com>
2023-07-17 18:41:32 +0200

    Removed extra ones interfaces

--- src/main/java/ua/foxminded/university/repo/StudentRepo.java
+++
@@ -1,8 +0,0 @@
-package ua.foxminded.university.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ua.foxminded.university.entity.Student;
-
-public interface StudentRepo extends JpaRepository<Student, Long> {
-    
-}
